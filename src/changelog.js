a8f464519146e88b6d9e7daace24ac8728ad8a6d
CanisMinor <canisminor1990@gmail.com>
2017-10-19 00:07:35 +0800

    update

--- src/changelog.js
+++ src/changelog.js
@@ -1,1 +1,5 @@
-export default ['2017.10.18||Todo: 添加设置页面', '2017.10.18||CanisMinor Act Skin v1.0'];
+export default [
+  '2017.10.19||Todo: 添加设置页面',
+  '2017.10.19||+ v1.0.1 增加右键菜单/折叠模式',
+  '2017.10.18||CanisMinor Act Skin v1.0.0',
+];
