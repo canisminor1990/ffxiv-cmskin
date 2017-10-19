7eb3cf0a00888a3cab7214851dce92bc807e62fb
canisminor1990 <canisminor1990@gmail.com>
2017-10-19 16:29:09 +0800

    add img

--- src/components/Avatar/index.js
+++ src/components/Avatar/index.js
@@ -8,10 +8,40 @@ export default ({ size = '2.5rem', deaths = 0, job }) => {
     height: size,
   };
 
+  const Jobs = [
+    'acn',
+    'arc',
+    'ast',
+    'blm',
+    'brd',
+    'cnj',
+    'drg',
+    'drk',
+    'gla',
+    'limitbreak',
+    'Inc',
+    'mch',
+    'mnk',
+    'mrd',
+    'nin',
+    'pet',
+    'pgl',
+    'pld',
+    'rdm',
+    'rog',
+    'sam',
+    'sch',
+    'smn',
+    'thm',
+    'war',
+    'whm',
+  ];
+  const Job = Jobs.indexOf(job) === -1 ? 'default' : job;
+
   return (
     <div className={style.avatar} style={avatarStyle}>
       {deaths > 0 ? <div className={style.deaths}>✗{deaths}</div> : null}
-      <img src={path.join('/img/jobs', job + '.png')} />
+      <img src={path.join('/img/jobs', Job + '.png')} />
     </div>
   );
 };
