af755d1f895c96b236aab233400d35824727ea99
canisminor1990 <canisminor1990@gmail.com>
2017-11-13 12:14:37 +0800

    fix

--- publish.sh
+++ publish.sh
@@ -1,10 +1,26 @@
 #!/bin/bash
-yarn build
+
+# Build
+read -p "Nead Build y/n: " BUILD
+if [ "BUILD" = "y" ]
+then
+	yarn build
+fi
+
 git add -A
 git tag
+
+# Commit && Tag
 read -p "Enter version: " VERSION
 read -p "Enter commit msg: " COMMIT
-git commit -m '$VERSION $COMMIT'
-yarn version --new-version $VERSION
-git push
-git push --tag
+
+if [ "$VERSION" ]
+then
+ git commit -m "$VERSION: $COMMIT"
+ yarn version --new-version $VERSION
+ git push
+ git push --tag
+else
+ git commit -m "$COMMIT"
+ git push
+fi
