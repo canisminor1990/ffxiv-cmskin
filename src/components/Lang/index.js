fe0aa67dd1106fff25d14a52d9db4928b39d83b0
CanisMinor <canisminor1990@gmail.com>
2017-11-13 20:07:29 +0800

    fix

--- src/components/Lang/index.js
+++ src/components/Lang/index.js
@@ -2,6 +2,7 @@ import langData from '../../lang';
 
 export default ({ id }) => {
   try {
+    if (!window.lang) window.lang = 'cn';
     return langData[window.lang][id].toString();
   } catch (e) {
     console.log('No lang:', id);
