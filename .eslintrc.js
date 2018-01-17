d97c5e0be91e94c095e4924586159f52589c20bc
canisminor1990 <canisminor1990@gmail.com>
2018-01-17 18:00:47 +0800

    update

--- .eslintrc.js
+++ .eslintrc.js
@@ -47,3 +47,4 @@ module.exports = {
     'react/no-children-prop': [0]
   }
 };
+
--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,5 @@
 import gulp from 'gulp';
-import htmlmin from 'gulp-htmlmin';
+import htmlbeautify from 'gulp-html-beautify';
 import { create } from 'browser-sync';
 import historyApiFallback from 'connect-history-api-fallback';
 import Changelog from './script/changelog';
@@ -9,12 +9,19 @@ import nga from './script/nag';
 gulp.task('changelog', Changelog);
 gulp.task('ngalog', Ngalog);
 gulp.task('nga', nga);
-gulp.task('html', () => gulp.src('dist/*.html')
-                            .pipe(htmlmin({collapseWhitespace: true}))
-                            .pipe(gulp.dest('dist')));
-gulp.task('test', () => create.init({
-	                                    server: {
-		                                    baseDir   : './dist',
-		                                    middleware: [historyApiFallback()]
-	                                    }
-                                    }));
+gulp.task('html', () => {
+  const options = { indentSize: 2 };
+  gulp
+    .src('./dist/*.html')
+    .pipe(htmlbeautify(options))
+    .pipe(gulp.dest('./dist/'));
+});
+gulp.task('test', () => {
+  const options = {
+    server: {
+      baseDir: './dist',
+      middleware: [historyApiFallback()],
+    },
+  };
+  create.init(options);
+});
