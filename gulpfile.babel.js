3706c6dd3adb5d24530643422b80773ff9af0cab
canisminor1990 <canisminor1990@gmail.com>
2018-01-17 17:44:19 +0800

    update

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,18 +1,20 @@
 import gulp from 'gulp';
+import htmlmin from 'gulp-htmlmin';
+import { create } from 'browser-sync';
+import historyApiFallback from 'connect-history-api-fallback';
 import Changelog from './script/changelog';
 import Ngalog from './script/ngalog';
 import nga from './script/nag';
-import browserSync from 'browser-sync';
-import historyApiFallback from 'connect-history-api-fallback';
 
 gulp.task('changelog', Changelog);
 gulp.task('ngalog', Ngalog);
 gulp.task('nga', nga);
-gulp.task('test', () => {
-	browserSync.create().init({
-		                          server: {
-			                          baseDir: './dist',
-			                          middleware: [ historyApiFallback() ]
-		                          }
-	                          });
-});
+gulp.task('html', () => gulp.src('dist/*.html')
+                            .pipe(htmlmin({collapseWhitespace: true}))
+                            .pipe(gulp.dest('dist')));
+gulp.task('test', () => create.init({
+	                                    server: {
+		                                    baseDir   : './dist',
+		                                    middleware: [historyApiFallback()]
+	                                    }
+                                    }));
