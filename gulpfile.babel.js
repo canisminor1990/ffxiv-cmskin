59cfd8e1db4573db7311f9e2fecdfd38529db311
canisminor1990 <canisminor1990@gmail.com>
2018-02-11 13:40:46 +0800

    sync

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,5 +1,7 @@
 import gulp from 'gulp';
 import htmlbeautify from 'gulp-html-beautify';
+import fs from 'fs-extra';
+import path from 'path';
 import { create } from 'browser-sync';
 import historyApiFallback from 'connect-history-api-fallback';
 import Changelog from './script/changelog';
@@ -10,18 +12,37 @@ gulp.task('changelog', Changelog);
 gulp.task('ngalog', Ngalog);
 gulp.task('nga', nga);
 gulp.task('html', () => {
-  const options = { indentSize: 2 };
-  gulp
-    .src('./dist/*.html')
-    .pipe(htmlbeautify(options))
-    .pipe(gulp.dest('./dist/'));
+	const options = {indentSize: 2};
+	gulp
+		.src('./dist/*.html')
+		.pipe(htmlbeautify(options))
+		.pipe(gulp.dest('./dist/'));
 });
 gulp.task('test', () => {
-  const options = {
-    server: {
-      baseDir: './dist',
-      middleware: [historyApiFallback()],
-    },
-  };
-  create.init(options);
+	const options = {
+		server: {
+			baseDir   : './dist',
+			middleware: [historyApiFallback()]
+		}
+	};
+	create.init(options);
+});
+
+gulp.task('deploy', () => {
+	const routesDir = [
+		'team',
+		'history',
+		'qrcode',
+		'detail',
+		'setting',
+		'setting/overlay',
+		'setting/team',
+		'setting/history',
+		'setting/detail',
+		'setting/qrcode',
+		'setting/setting'
+	];
+	routesDir.forEach((route) => {
+		fs.copySync('docs/index.html', path.join('docs', route, 'index.html'));
+	});
 });
