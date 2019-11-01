80b1918621f3df386d8cb216ebf2f909aaedd437
SgLy <775150558@qq.com>
2019-11-02 05:02:10 +0800

    fix(typo): otehr -> other

--- src/components/Icon/index.js
+++ src/components/Icon/index.js
@@ -3,14 +3,14 @@ import { Link } from 'dva/router';
 import { Icon as AntIcon } from 'antd';
 import style from './index.scss';
 
-export default ({ to, type, ...otehr }) => {
+export default ({ to, type, ...other }) => {
   return (
     <Link
       to={to}
       className={classnames.bind(style)('icon', {
         active: window.location.pathname === to,
       })}
-      {...otehr}
+      {...other}
     >
       <AntIcon type={type} />
     </Link>
