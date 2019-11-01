1bd669b4ba92483eded25acab9688aca52577521
SgLy <775150558@qq.com>
2019-11-02 05:05:46 +0800

    fix(compile): fix outdated compile chain

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -9,19 +9,20 @@ import nga from './script/nag';
 gulp.task('changelog', Changelog);
 gulp.task('ngalog', Ngalog);
 gulp.task('nga', nga);
-gulp.task('html', () => {
-	const options = {indentSize: 2};
-	gulp
-		.src('./docs/*.html')
-		.pipe(htmlbeautify(options))
-		.pipe(gulp.dest('./docs/'));
+gulp.task('html', done => {
+  const options = { indentSize: 2 };
+  gulp
+    .src('./docs/*.html')
+    .pipe(htmlbeautify(options))
+    .pipe(gulp.dest('./docs/'));
+  done();
 });
 gulp.task('test', () => {
-	const options = {
-		server: {
-			baseDir   : './docs',
-			middleware: [historyApiFallback()]
-		}
-	};
-	create.init(options);
+  const options = {
+    server: {
+      baseDir: './docs',
+      middleware: [historyApiFallback()],
+    },
+  };
+  create.init(options);
 });
